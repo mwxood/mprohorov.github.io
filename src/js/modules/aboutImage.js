c1acede703362b4a7ec54da360172a2db075d79e
Mihail <mwxood@gmail.com>
2021-05-30 15:21:37 +0300

    dada

--- src/js/modules/aboutImage.js
+++
@@ -1,9 +0,0 @@
-const aboutImage = () => {
-    const url = 'https://robohash.org/set_set';
-    const imageHolder = document.querySelector('.image-holder');
-    const image = document.createElement ('img');
-    image.src = url + Math.round (Math.random () * 10);
-    imageHolder.append (image);
-};
-
-export default aboutImage;
